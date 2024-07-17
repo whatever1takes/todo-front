afc7f509f2cb831c63ba806ce4b9d3e458947927
周文浩 <75059345+whatever1takes@users.noreply.github.com>
2024-07-17 16:53:35 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="todo-app">
+    shiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiit
+    <TodoList></TodoList>
+
+  </div>
+</template>
+
+<script setup>
+import TodoList from './components/TodoList.vue'
+</script>
+
+<style scoped></style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="todo-app">
+        <el-card shadow="always" :body-style="{ padding: '20px' }" style="max-width: 480px">
+            <template #header>
+                <div class="todo-header">
+                    待办事项
+                </div>
+            </template>
+            <div class="todo-input">
+
+                <el-input v-model="newTodo" placeholder="新建代办事项..."></el-input>
+                <el-button type="primary" @click="addTodo">添加</el-button>
+            </div>
+
+        </el-card>
+        <el-card style="max-width: 480px">
+            <template #header>
+                <div class="card-header">
+                    <span>Card name</span>
+                </div>
+            </template>
+            <p v-for="o in 4" :key="o" class="text item">{{ 'List item ' + o }}</p>
+            <template #footer>Footer content</template>
+        </el-card>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped></style>
